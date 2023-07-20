3416c8e3ab553d9c55719ce4ced62987849458e7
김지형 <jihyoung9912@gmail.com>
2023-07-20 12:28:29 -0700

    chore: Color Palette

--- src/constants/COLOR.ts
+++ src/constants/COLOR.ts
@@ -1,8 +1,13 @@
 export const COLORS = {
-  primary: '#5184B1',
-  primaryVariant: '#98B2C9',
-  secondary: '#E77853',
-  secondaryVariant: '#FF7854',
-  Tertiary: '#E9DECF',
-  gray: '#ccc',
+  backgorundPrimary: '#e6e6e6',
+  backgroundSecondary: '#f5f7f9',
+
+  fontPrimary: '#66696B',
+  fontSecondary: '#325757',
+  fontTertiary: '#4B2C34',
+
+  buttonPrimary: '#7FA47F',
+  buttonPrimaryVariant: '#5F795F',
+  buttonSecondary: '#447878',
+  buttonSecondaryVariant: '#325B5B',
 };
