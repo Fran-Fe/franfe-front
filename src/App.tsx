e9d0ab94c75bcf02622b634a5b1cd31eb18a59d4
김지형 <jihyoung9912@gmail.com>
2023-07-20 14:56:19 -0700

    style: apply GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import theme from 'styles/theme';
 import { Home, Compare, Gallery, Rank } from 'pages';
+import { GlobalStyles, theme } from 'styles';
 
 const App = () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/compare" element={<Compare />} />
---
+++ src/styles/index.ts
@@ -0,0 +1,2 @@
+export { default as GlobalStyles } from 'styles/GlobalStyles';
+export { default as theme } from 'styles/theme';
