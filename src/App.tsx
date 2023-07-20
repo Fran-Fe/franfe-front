9a7c6af3663d8f3fee28b4cdbfc9c6a43717b327
김지형 <jihyoung9912@gmail.com>
2023-07-20 14:35:54 -0700

    chore: include all provider in App.tsx not index

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
 import { Home, Compare, Gallery, Rank } from 'pages';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/compare" element={<Compare />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/rank" element={<Rank />} />
-    </Routes>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/compare" element={<Compare />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/rank" element={<Rank />} />
+        </Routes>
+      </ThemeProvider>
+    </Router>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import theme from 'themes/theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
