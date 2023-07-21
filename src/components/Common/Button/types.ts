b593207ec11f2e62e4a889f426ebea9eb1df1ece
김지형 <jihyoung9912@gmail.com>
2023-07-21 16:14:50 -0700

    feat: Reusablle Button

---
+++ src/components/Common/Button/types.ts
@@ -0,0 +1,32 @@
+import { ElementType } from 'react';
+import { LayoutProps, SpaceProps } from 'styled-system';
+import { PolymorphicComponentProps } from './polymorphic';
+
+export const scales = {
+  MD: 'md',
+  SM: 'sm',
+  XS: 'xs',
+} as const;
+
+export const variants = {
+  PRIMARY: 'primary',
+  PRIMARY_BASIC: 'primaryBasic',
+  SECONDARY: 'secondary',
+  SECONDARY_BASIC: 'secondaryBasic',
+  TEXT: 'text',
+} as const;
+
+export type Scale = (typeof scales)[keyof typeof scales];
+export type Variant = (typeof variants)[keyof typeof variants];
+
+export interface BaseButtonProps extends LayoutProps, SpaceProps {
+  isLoading?: boolean;
+  scale?: Scale;
+  variant?: Variant;
+  disabled?: boolean;
+}
+
+export type ButtonProps<P extends ElementType = 'button'> = PolymorphicComponentProps<
+  P,
+  BaseButtonProps
+>;
