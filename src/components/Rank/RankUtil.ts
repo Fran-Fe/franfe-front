3f7104d6e2cee065b27d50e7fd0ee799f9c38d57
SungJun <jmin9480@naver.com>
2023-07-31 11:38:10 -0700

    feat: Inital Ranking Page Design

---
+++ src/components/Rank/RankUtil.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const RankBackButton = styled.img`
+  width: 8%;
+`;
+
+export const RankHeadText = styled.div`
+  display: flex;
+  align-items: end;
+  justify-content: center;
+  height: 100%;
+  padding: 0 1rem 0 0;
+  font-size: 1.5rem;
+`;
---
+++ src/components/Rank/index.ts
@@ -0,0 +1,4 @@
+export { default as Ranking } from './Ranking';
+export { RankingLayouts, RankingHeader, RankingContainer, RankingText } from './RankingLayout';
+export { RankBackButton, RankHeadText } from './RankUtil';
+export { default as RankingCard } from './RankingCard';
