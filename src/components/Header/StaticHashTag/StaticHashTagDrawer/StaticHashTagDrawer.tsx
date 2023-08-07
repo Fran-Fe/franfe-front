b5d23e7cb7f74d7902435c90c8972ceac2c564cb
SungJun <jmin9480@naver.com>
2023-08-07 16:18:17 -0700

    fix: Seperated Utils

--- src/components/Header/StaticHashTag/StaticHashTagDrawer/StaticHashTagDrawer.tsx
+++ src/components/Header/StaticHashTag/StaticHashTagDrawer/StaticHashTagDrawer.tsx
@@ -1,85 +1,16 @@
-import styled from 'styled-components';
-import { Button, Drawer } from 'components/Common';
-import { HashtagIconButton } from 'components/HashtagIcon';
-
-interface IDrawerVisble {
-  isOpen: boolean;
-  onClose: () => void;
-}
-
-const StaticHashtagDrawerContainer = styled.div`
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  align-content: flex-start;
-  width: 100%;
-  height: 100%;
-`;
-
-const BackButtonContainer = styled.div`
-  position: sticky;
-  top: -2px;
-  width: 100%;
-  height: 8%;
-  background-color: #fff;
-`;
-
-const StaticHashtagBackButton = styled.img`
-  width: 8%;
-  transform: rotate(90deg);
-`;
-
-const StaticHashtagIconsContainer = styled.div`
-  position: sticky;
-  top: 7.5%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 0.5rem;
-  width: 100%;
-  background-color: #fff;
-`;
-
-const StaticHashtagIconButton = styled(HashtagIconButton)`
-  width: 20%;
-  height: 2rem;
-  border: 0.2rem solid ${(props) => props.theme.buttonPrimary};
-`;
-
-const DynamicHashtagIconsContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-content: flex-start;
-  justify-content: center;
-  margin-top: 0.5rem;
-  gap: 0.7rem;
-`;
-
-const DynamicHashtagIconButton = styled(HashtagIconButton)`
-  width: 16%;
-`;
-
-const StaticHashTagDrawerFooter = styled.div`
-  position: sticky;
-  top: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  padding: 0.5rem 1rem;
-`;
-
-const DrawerButton = styled(Button)`
-  width: 50%;
-  height: 100%;
-  margin: 0.5rem auto;
-  border-radius: 20px;
-  text-transform: uppercase;
-  font-size: 1.5rem;
-  color: ${(props) => props.theme.fontPrimary};
-  background: #fff;
-`;
+import { Drawer } from 'components/Common';
+import {
+  IDrawerVisble,
+  StaticHashtagDrawerContainer,
+  BackButtonContainer,
+  StaticHashtagBackButton,
+  StaticHashtagIconsContainer,
+  StaticHashtagIconButton,
+  DynamicHashtagIconsContainer,
+  DynamicHashtagIconButton,
+  StaticHashTagDrawerFooter,
+  DrawerButton,
+} from './StaticHashTagDrawerUtils';
 
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 7);
---
+++ src/components/Header/StaticHashTag/StaticHashTagDrawer/StaticHashTagDrawerUtils.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { Button } from 'components/Common';
+import { HashtagIconButton } from 'components/HashtagIcon';
+
+export interface IDrawerVisble {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const StaticHashtagDrawerContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  width: 100%;
+  height: 100%;
+`;
+
+export const BackButtonContainer = styled.div`
+  position: sticky;
+  top: -2px;
+  width: 100%;
+  height: 8%;
+  background-color: #fff;
+`;
+
+export const StaticHashtagBackButton = styled.img`
+  width: 8%;
+  transform: rotate(90deg);
+`;
+
+export const StaticHashtagIconsContainer = styled.div`
+  position: sticky;
+  top: 7.5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  width: 100%;
+  background-color: #fff;
+`;
+
+export const StaticHashtagIconButton = styled(HashtagIconButton)`
+  width: 20%;
+  height: 2rem;
+  border: 0.2rem solid ${(props) => props.theme.buttonPrimary};
+`;
+
+export const DynamicHashtagIconsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: center;
+  margin-top: 0.5rem;
+  gap: 0.7rem;
+`;
+
+export const DynamicHashtagIconButton = styled(HashtagIconButton)`
+  width: 16%;
+`;
+
+export const StaticHashTagDrawerFooter = styled.div`
+  position: sticky;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 0.5rem 1rem;
+`;
+
+export const DrawerButton = styled(Button)`
+  width: 50%;
+  height: 100%;
+  margin: 0.5rem auto;
+  border-radius: 20px;
+  text-transform: uppercase;
+  font-size: 1.5rem;
+  color: ${(props) => props.theme.fontPrimary};
+  background: #fff;
+`;
